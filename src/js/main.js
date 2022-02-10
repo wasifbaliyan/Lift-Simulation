f41dc4066c1afcca1eb2aae6ef0e29eb086732a7
Wasif Baliyan <w.baliyan@gmail.com>
2022-02-10 23:27:28 +0530

    write functions for creating floors and lifts of a single floor, findNearbyLift, moveLift up and down, createUI

---
+++ src/js/main.js
@@ -0,0 +1,95 @@
+const lifts = document.getElementById("lifts");
+const floors = document.getElementById("floors");
+const root = document.getElementById("root");
+const createFloorAndLiftsButton = document.getElementById(
+  "create-floor-and-lifts"
+);
+let liftsFloors;
+// create floors and no of of lifts on floor
+const createLiftsAndFloors = (totalFloors, liftsPerFloor) => {
+  const floorLiftMatrix = [];
+  for (let i = 0; i < totalFloors; i++) {
+    const liftsOnFloor = [];
+    for (let j = 0; j < liftsPerFloor; j++) {
+      if (i === 0) {
+        liftsOnFloor.push(1);
+      } else {
+        liftsOnFloor.push(0);
+      }
+    }
+    floorLiftMatrix.push(liftsOnFloor);
+  }
+  return floorLiftMatrix;
+};
+
+const nearbyLift = (floorIndex, liftsAndFloorsArray) => {
+  //check if there is any lift on the current floor
+  // for (let i = 0; i < liftsAndFloorsArray[floorIndex].length; i++) {
+  //   if (liftsAndFloorsArray[floorIndex][i] === 1) {
+  //     console.log("AT FLOOR", [floorIndex, i]);
+  //     return [floorIndex, i];
+  //   }
+  // }
+
+  /* check if nearyby floors for lifts[check for the direction where no of floors are less] 
+ for ex: current floor is 3, total floor are 9 first we go for the bottom and above loop */
+  for (let i = 0; i < liftsAndFloorsArray.length; i++) {
+    for (let j = 0; j < liftsAndFloorsArray[i].length; j++) {
+      if (liftsAndFloorsArray[i][j] === 1) {
+        console.log("FOUND FLOOR", [i, j]);
+        return [i, j];
+      }
+    }
+  }
+};
+
+const moveLiftUpOrDown = (floorIndex) => {
+  liftsFloors = createLiftsAndFloors(Number(floors.value), Number(lifts.value));
+  const foundNearbyLift = nearbyLift(floorIndex, liftsFloors);
+
+  liftsFloors[foundNearbyLift[0]][foundNearbyLift[1]] = 0;
+  liftsFloors[floorIndex][foundNearbyLift[1]] = 1;
+  root.innerHTML = null;
+  createUI(liftsFloors);
+};
+
+// console.log(
+//   moveLiftUpOrDown(2, [
+//     [0, 1, 0],
+//     [0, 0, 0],
+//     [1, 0, 0],
+//     [0, 0, 1],
+//   ])
+// );
+createFloorAndLiftsButton.addEventListener("click", () => {
+  liftsFloors = createLiftsAndFloors(Number(floors.value), Number(lifts.value));
+  createUI(liftsFloors);
+});
+
+function createUI(liftAndFloorArray) {
+  for (let i = 0; i < liftAndFloorArray.length; i++) {
+    let div = document.createElement("div");
+    div.style.height = "200px";
+    div.style.margin = "1rem";
+    div.style.display = "flex";
+    div.style.borderBottom = "2px solid #ccc";
+    div.innerHTML = `<div><span>Floor ${i}</span><button onclick="moveLiftUpOrDown(${i})" class="floor-btn">Up</button><button onclick="moveLiftUpOrDown(${i})" class="floor-btn">Down</button></div>`;
+    for (let j = 0; j < liftAndFloorArray[i].length; j++) {
+      if (liftAndFloorArray[i][j] === 1) {
+        let innerDiv = document.createElement("div");
+        innerDiv.style.height = "100%";
+        innerDiv.style.width = "100px";
+        innerDiv.style.backgroundColor = "blue";
+        innerDiv.style.margin = "0 1rem";
+        div.appendChild(innerDiv);
+      } else {
+        let innerDiv = document.createElement("div");
+        innerDiv.style.height = "100%";
+        innerDiv.style.width = "100px";
+        innerDiv.style.margin = "0 1rem";
+        div.appendChild(innerDiv);
+      }
+    }
+    root.appendChild(div);
+  }
+}
